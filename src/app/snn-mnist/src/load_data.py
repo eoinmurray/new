ee8308be7e49845acc7cb1db9285d101e5bb73fc
Eoin Murray <me@eoinmurray.info>
2025-04-19 16:10:22 +0200

    stash

--- src/app/snn-mnist/src/load_data.py
+++ src/app/snn-mnist/src/load_data.py
@@ -1,14 +1,18 @@
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, FashionMNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-def load_mnist_data(batch_size=128):
+def load_mnist_data(batch_size=128, dataset='MNIST'):
     transform = transforms.Compose([transforms.ToTensor()])
     
-    # Load datasets
-    train_dataset = MNIST(root=".", train=True, download=True, transform=transform)
-    test_dataset = MNIST(root=".", train=False, download=True, transform=transform)
-    
+    if dataset == 'MNIST':
+        dataset = MNIST
+    elif dataset == 'FashionMNIST':
+        dataset = FashionMNIST
+
+    train_dataset = dataset(root=".", train=True, download=True, transform=transform)
+    test_dataset = dataset(root=".", train=False, download=True, transform=transform)
+
     # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
